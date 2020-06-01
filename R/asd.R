bb9cfd5d9d1890ce1097da21bbabc171d748b0fe
Atakan Kızıltan <m.atakan.kiziltan@gmail.com>
2020-06-01 20:38:44 +0300

    Update.

--- R/asd.R
+++ R/asd.R
@@ -3,7 +3,6 @@
 #' @description You can reach instant bad road information. See \url{https://data.ibb.gov.tr/organization/iett-genel-mudurlugu} for more information.
 #' @export
 #'
-#' @param busdoornumber This parameter is not mandatory. This information is the information that comes to the bus driver screens. For this reason, you can also query bad roads by the bus door number.
 #'
 #' @return NULL
 #' @importFrom jsonlite fromJSON
